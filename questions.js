8e72adbf8eda30291eca20a3f46b59e93c8d8f1d
Rodrigues da Silva <rodriguesdasilva@MacBook-Pro-de-Rodrigues.local>
2024-07-29 11:54:52 +0200

    javascript

---
+++ questions.js
@@ -0,0 +1,108 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Qu'est-ce que Windows 10?",
+    answer: "B. Un sytéme d'explotation",
+    options: [
+      "A. Un fichier",
+      "B. Un sytéme d'explotation",
+      "C. Un logiciel de traitement de texte",
+      "D. Un logiciel tableur",
+    ],
+  },
+  {
+    numb: 2,
+    question: "Quel est le nom de l'icône entourée en rouge dans l'image?",
+    answer: "B. Explorateur de Windows",
+    options: ["A. Word", "B. Explorateur de Windows", "C. Excel", "D. Windows"],
+  },
+  {
+    numb: 3,
+    question: "Quel est le nom de l'icône entourée en rouge dans l'image?",
+    answer: "D. Microsoft Word",
+    options: [
+      "A. Windows",
+      "B. Google Chrome",
+      "C. Microsoft Edge",
+      "D. Microsoft Word",
+    ],
+  },
+  {
+    numb: 4,
+    question: "Dans l'image, quelle application est un raccourci ?",
+    answer: "A. Google Chrome",
+    options: ["A. Google Chrome", "B. Word", "C. Excel", "D. Outlook"],
+  },
+  {
+    numb: 5,
+    question: "Comment épingler une application dans la barre des tâches?",
+    answer: "A. Clic droit sur l’application, épinglé à la barre des tâches",
+    options: [
+      "A. Clic droit sur l’application, épinglé à la barre des tâches",
+      "B. Clic gauche sur l’application, épinglé à la barre des tâches",
+      "C. Double Clic gauche sur l’application, épinglé à la barre des tâches",
+      "D. Double Clic droit sur l’application, épinglé à la barre des tâches",
+    ],
+  },
+  {
+    numb: 6,
+    question:
+      "Comment supprimer un fichier de façon définitive dans l’explorateur Windows?",
+    answer:
+      "B. Bouton supprimer, option supprimer un fichier de façon définitive",
+    options: [
+      "A. Bouton supprimé",
+      "B. Bouton supprimer, option supprimer un fichier de façon définitive",
+      "C. Bouton supprimer option placer dans la corbeille",
+      "D. Bouton supprimé dans la corbeille",
+    ],
+  },
+  {
+    numb: 7,
+    question: "Comment dupliquer un fichier dans l’explorateur Windows?",
+    answer: "A. Bouton copier et coller",
+    options: [
+      "A. Bouton copier et coller",
+      "B. Bouton couper et coller",
+      "C. Bouton coller et copier",
+      "D. Bouton coller et couper",
+    ],
+  },
+  {
+    numb: 8,
+    question: "Comment récupère un fichier qui se trouve dans la corbeille?",
+    answer: "B. Dans la corbeille, restaurer les éléments sélectionnés",
+    options: [
+      "A. Dans la corbeille, restaurer tous les éléments",
+      "B. Dans la corbeille, restaurer les éléments sélectionnés",
+      "C. Dans la corbeille, vider la corbeille",
+      "D. Dans la corbeille, récupère un élément",
+    ],
+  },
+  {
+    numb: 9,
+    question: "Parmi les réponses laquelle n’est pas une fichier image?",
+    answer: "D. Image.pdf",
+    options: ["A. Image.jpeg", "B. Image.gif", "C. Image.png", "D. Image.pdf"],
+  },
+  {
+    numb: 10,
+    question:
+      "Comment s'appelle le bouton situé à gauche de la barre de tâche?",
+    answer: "D. Le menu démarrer",
+    options: [
+      "A. Le bouton marche",
+      "B. Le bouton de Windows",
+      "C. Le bouton arrêt",
+      "D. Le menu démarrer",
+    ],
+  },
+
+  {
+    numb: 11,
+    question:
+      "Comment appel-t-on l'endroit où sont stockés les fichiers supprimés?",
+    answer: "B. La corbeille",
+    options: ["A. La poubelle", "B. La corbeille", "C. Le tri", "D. La boite"],
+  },
+];
---
+++ script.js
@@ -0,0 +1,51 @@
+const startBtn = document.querySelector(".start-btn");
+const popupInfo = document.querySelector(".popup-info");
+const exitBtn = document.querySelector(".exit-btn");
+const main = document.querySelector(".main");
+const continueBtn = document.querySelector(".continue-btn");
+const quizSection = document.querySelector(".quiz-section");
+const quizBox = document.querySelector(".quiz-box");
+
+startBtn.onclick = () => {
+  popupInfo.classList.add("active");
+  main.classList.add("active");
+};
+
+exitBtn.onclick = () => {
+  popupInfo.classList.remove("active");
+  main.classList.remove("active");
+};
+
+continueBtn.onclick = () => {
+  quizSection.classList.add("active");
+  popupInfo.classList.remove("active");
+  main.classList.remove("active");
+  quizBox.classList.add("active");
+
+  showQuestions(0);
+};
+
+let questionCount = 0;
+
+const nextBtn = document.querySelector(".next-btn");
+
+nextBtn.onclick = () => {
+  questionCount++;
+  showQuestions(questionCount);
+};
+
+const optionList = document.querySelector(".option-list");
+
+//getting questions and optiond from array
+
+function showQuestions(index) {
+  const questionText = document.querySelector(".question-text");
+  questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+  let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+    <div class="option"><span>${questions[index].options[1]}</span></div>
+    <div class="option"><span>${questions[index].options[2]}</span></div>
+    <div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+  optionList.innerHTML = optionTag;
+}

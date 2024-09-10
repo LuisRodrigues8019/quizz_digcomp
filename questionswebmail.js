457ee06db9f79f02f8ff1787d5adc4a0bf8c7142
Rodrigues da Silva <rodriguesdasilva@MacBook-Pro-de-Rodrigues.local>
2024-09-10 12:13:26 +0200

    quiz webmail

---
+++ questionswebmail.js
@@ -0,0 +1,92 @@
+const questions = [
+  {
+    numb: 1,
+    question: "Qu’est ce qu'un Webmail ?",
+    answer:
+      "A. Un service permettant de consulter et envoyer des emails via un navigateur",
+    options: [
+      "A. Un service permettant de consulter et envoyer des emails via un navigateur",
+      "B. Un réseau social pour partager des vidéos",
+      "C. Un logiciel pour éditer des documents en ligne",
+      "D. Une plateforme de messagerie instantanée",
+    ],
+  },
+  {
+    numb: 2,
+    question: "Qu'est-ce qu'un SPAM ?",
+    answer: "C. Un courrier électronique non sollicité",
+    options: [
+      "A. Un fichier joint sécurisé",
+      "B. Un message d'erreur réseau",
+      "C. Un courrier électronique non sollicité",
+      "D. Un code utilisé pour accéder à un compte",
+    ],
+  },
+  {
+    numb: 3,
+    question:
+      "Dans l'image, quel onglet du navigateur correspond à un service de Webmail ?",
+    answer: "B. Onglet 1",
+    options: ["A. Onglet 2", "B. Onglet 1", "C. Onglet 3", "D. Onglet 4"],
+    image: "images/webmailquestion.png",
+  },
+  {
+    numb: 4,
+    question: "Quel est le statut des adresses insérées dans le champ Cci ?",
+    answer: "B. Adresse d'un destinataire en copie invisible pour les autres",
+    options: [
+      "A. Les adresses sont visibles par tout le monde",
+      "B. Adresse d'un destinataire en copie invisible pour les autres",
+      "C. Les adresses sont envoyées en priorité",
+      "D. Les adresses reçoivent une notification spéciale",
+    ],
+  },
+  {
+    numb: 5,
+    question: "Quel est le nom du protocole permettant d'envoyer un e-mail ?",
+    answer: "A. SMTP",
+    options: ["A. SMTP", "B. IMAP", "C. HTTP", "D. FTP"],
+  },
+  {
+    numb: 6,
+    question: "Qu’est-ce qui est toujours présent dans une adresse e-mail ?",
+    answer: "D. Le symbole @",
+    options: [
+      "A. Un numéro de téléphone",
+      "B. Un point d'exclamation",
+      "C. Une adresse postale",
+      "D. Le symbole @",
+    ],
+  },
+  {
+    numb: 7,
+    question: "Dans l'image, de quel type de mail s'agit-il ?",
+    answer: "A. Email phishing",
+    options: [
+      "A. Email phishing",
+      "B. Email de confirmation de commande",
+      "C. Email de newsletter",
+      "D. Email personnel",
+    ],
+    image: "images/phishing.png",
+  },
+  {
+    numb: 8,
+    question:
+      "Quel est l'objectif principal du protocole POP3 dans la gestion des emails ?",
+    answer: "B. Télécharger les emails du serveur vers un seul appareil",
+    options: [
+      "A. Envoyer des emails",
+      "B. Télécharger les emails du serveur vers un seul appareil",
+      "C. Synchroniser les emails sur plusieurs appareils",
+      "D. Gérer les emails dans le cloud",
+    ],
+  },
+  {
+    numb: 9,
+    question:
+      "Quel protocole est utilisé pour synchroniser les emails entre un serveur et plusieurs appareils ?",
+    answer: "C. IMAP",
+    options: ["A. POP3", "B. SMTP", "C. IMAP", "D. HTTP"],
+  },
+];
